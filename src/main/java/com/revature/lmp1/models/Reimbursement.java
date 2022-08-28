cf4a1ed62ddfa2f112495e9c5b2205d36325643b
Leighton Albrecht <leightonalbrecht@gmail.com>
2022-08-28 15:14:59 -0400

    created user and reimbursement models

---
+++ src/main/java/com/revature/lmp1/models/Reimbursement.java
@@ -0,0 +1,126 @@
+package com.revature.lmp1.models;
+
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+public class Reimbursement {
+    public String id;
+    public int amount;
+    public LocalDateTime submitted;
+    public LocalDateTime resolved;
+    public String description;
+    public Blob receipt;
+    public String paymentId;
+    public String authorId;
+    public String resolverId;
+    public String statusId;
+    public String typeId;
+
+    public Reimbursement() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getSubmitted() {
+        return submitted;
+    }
+
+    public void setSubmitted(LocalDateTime submitted) {
+        this.submitted = submitted;
+    }
+
+    public LocalDateTime getResolved() {
+        return resolved;
+    }
+
+    public void setResolved(LocalDateTime resolved) {
+        this.resolved = resolved;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Blob getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(Blob receipt) {
+        this.receipt = receipt;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getResolverId() {
+        return resolverId;
+    }
+
+    public void setResolverId(String resolverId) {
+        this.resolverId = resolverId;
+    }
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "id='" + id + '\'' +
+                ", amount=" + amount +
+                ", submitted=" + submitted +
+                ", resolved=" + resolved +
+                ", description='" + description + '\'' +
+                ", receipt=" + receipt +
+                ", paymentId='" + paymentId + '\'' +
+                ", authorId='" + authorId + '\'' +
+                ", resolverId='" + resolverId + '\'' +
+                ", statusId='" + statusId + '\'' +
+                ", typeId='" + typeId + '\'' +
+                '}';
+    }
+}
