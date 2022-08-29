55eeed7ce30ec62d3af2fd2b90d75f1de2f17100
mlopez44 <60417513+mlopez44@users.noreply.github.com>
2022-08-29 15:45:23 -0400

    Services

--- src/main/java/com/revature/lmp1/services/UserService.java
+++ src/main/java/com/revature/lmp1/services/UserService.java
@@ -1,4 +1,15 @@
 package com.revature.lmp1.services;
 
+import com.revature.lmp1.daos.UserDAO;
+
 public class UserService {
+
+    private final UserDAO userDAO;
+
+
+    public UserService(UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+
 }
